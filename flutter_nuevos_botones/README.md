1a72b8be4ebf7ab45c4b9b7bee4ef56f1158e912
Bryan <bestrada_23@uabcs.mx>
2025-11-21 17:59:57 -0700

    nuevos botones

---
+++ flutter_nuevos_botones/README.md
@@ -0,0 +1,3 @@
+# flutter_nuevos_botones
+
+A new Flutter project.
---
+++ flutter_nuevos_botones/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BotonPersonalizado(texto: 'Entrar', onPressed: null),
+              SizedBox(width: 20), // espacio entre botones
+              BotonPersonalizado(texto: 'Registrarse', onPressed: null),
+              SizedBox(width: 20), // espacio entre botones
+              BotonPersonalizado(texto: 'Salir', onPressed: null),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BotonPersonalizado extends StatelessWidget {
+  final String texto;
+  final VoidCallback? onPressed;
+
+  const BotonPersonalizado({super.key, required this.texto, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 0, 0, 0),
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xFF000000).withOpacity(0.2),
+              offset: const Offset(0, 4),
+              blurRadius: 6,
+            ),
+          ],
+        ),
+        child: Text(
+          texto,
+          style: const TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
